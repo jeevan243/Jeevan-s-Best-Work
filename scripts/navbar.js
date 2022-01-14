2e851e2bd349d1aca0d1d4a3014dc0fedc5e1cc8
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-01-14 20:46:58 +0530

    my best work

---
+++ scripts/navbar.js
@@ -0,0 +1,18 @@
+
+
+function navbar() {
+
+    return ` <div id="navbar">
+    <div id="home">
+      <h3><a href="search.html">Home</a></h3>
+    </div>
+
+    <div id="box">
+      <h3><a href="receipe.html">Random</a></h3>
+      <h3><a href="latest.html">Latest</a></h3>
+    </div>
+  </div>`
+}
+
+export default navbar;
+
